7dcf6c1f99ae10511e0d860ab5b843bc64a07ed3
EPROKB\PG11 <edmondwmngai@eprotel.com.hk>
2025-04-22 15:32:07 +0800

    Make sure the regex used here, which is vulnerable to super-linear runtime due to backtracking, cannot lead to denial of service.

--- js/SocialContent.js
+++ js/SocialContent.js
@@ -372,8 +372,8 @@ var SC = {
         replacedText = replacedText.replace(replacePattern2, '$1<a href="http://$2" target="_blank">$2</a>');
 
         //Change email addresses to mailto:: links. (updated for migration on 10-3-2025)
-        //replacePattern3 = /(([a-zA-Z0-9\-\_\.])+@[a-zA-Z\_]+?(\.[a-zA-Z]{2,6})+)/gim;
-        replacePattern3 = /[\w\.\-]+@[\w\-]+(\.[a-z]{2,6})+/gim;
+        replacePattern3 = /(([a-zA-Z0-9\-\_\.])+@[a-zA-Z\_]+?(\.[a-zA-Z]{2,6})+)/gim;
+        //replacePattern3 = /[\w\.\-]+@[\w\-]+(\.[a-z]{2,6})+/gim;
 
         replacedText = replacedText.replace(replacePattern3, '<a href="mailto:$1">$1</a>');
 
